76c6046483d61b715d5d954db07e6993049b076c
Vijeyakumar <65109903+Vijeyakumar26@users.noreply.github.com>
2021-01-30 21:53:24 +0530

    Created DBConnection.

---
+++ Java/Bank Management/src/com/htc/connection/DBConnection.java
@@ -0,0 +1,33 @@
+package com.htc.connection;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection 
+{
+	private String driverName;
+	private String url;
+	private String username;
+	private String password;
+	
+	public Connection getConnection() throws IOException, SQLException, ClassNotFoundException
+	{
+		FileReader fr = new FileReader("db.properties");
+		Properties prop = new Properties();
+		prop.load(fr);
+		
+		driverName = prop.getProperty("driverName");
+		url = prop.getProperty("url");
+		username = prop.getProperty("username");
+		password = prop.getProperty("password");
+		//register the driver class name
+		Class.forName(driverName);
+		//obtaining connection
+		Connection con = DriverManager.getConnection(url,username,password);
+		return con;
+	}
+}
