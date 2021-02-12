834426e57352e2b451899d3df5a173da404b4eb0
Vijeyakumar <65109903+Vijeyakumar26@users.noreply.github.com>
2021-02-12 17:31:57 +0530

    Created Interface UserDAO

---
+++ Adv Java/ServletDemo/src/com/htc/servlet/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.htc.servlet.dao;
+
+import com.htc.servlet.bo.User;
+
+public interface UserDAO
+{
+	public boolean addUser(User user);
+	public User getUser(String username);
+	public boolean deleteUser(String username);
+	public boolean updateUser(String username, String contactno);
+	public boolean validateUser(String username, String password);
+}
