8ff9a6bcebdaa876530ed58433c675a17ebc35e3
Vijeyakumar <65109903+Vijeyakumar26@users.noreply.github.com>
2021-02-26 20:27:30 +0530

    Created Employee Interface DAO & Implementation.

---
+++ Adv Java Part 2/springWebmvc/src/main/java/com/htc/springmvc/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.htc.springmvc.dao;
+
+import java.util.List;
+
+import com.htc.springmvc.model.Employee;
+
+public interface EmployeeDAO
+{
+	public boolean addEmployee(Employee emp);
+	public boolean addEmployee(int empno, String ename, String job, double salary);
+	public Employee getEmployee(int empno);
+	public List<Employee> getEmployees();
+	public boolean deleteEmployee(int empno);
+	public boolean updateEmployee(Employee newemp);
+}
